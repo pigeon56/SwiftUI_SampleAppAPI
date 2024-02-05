b1c339800342c5072b4772167440be338a376295
Cindy Michalowski <cindymichalowski.tech@gmail.com>
2024-02-05 16:37:55 -0500

    Initial commit; small app for mocking and ci/cd pipeline

---
+++ SampleAppAPI/ContentView.swift
@@ -0,0 +1,76 @@
+//
+//  ContentView.swift
+//  SampleAppAPI
+//
+//  Created by Cindy Michalowski on 2/5/24.
+//
+
+// tutorial: https://www.youtube.com/watch?v=CimY_Sr3gWw
+// no image caching implemented in this app
+// can abstract fetchData() loading function into a URLImageViewModel
+
+import SwiftUI
+
+struct URLImage: View {
+    
+    let urlString: String
+    
+    @State var data: Data?
+    
+    var body: some View {
+        if let data = data, let uiimage = UIImage(data: data) {
+            Image(uiImage: uiimage)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 130, height: 70)
+                .background(Color.gray)
+        } else {
+            Image(systemName: "video")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 130, height: 70)
+                .background(Color.gray)
+                .onAppear {
+                    fetchData()
+                }
+        }
+    }
+    
+    private func fetchData() {
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            self.data = data
+        }
+        task.resume()
+    }
+}
+
+struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(viewModel.courses, id: \.self) { course in
+                    HStack {
+                        URLImage(urlString: course.image)
+                        Text(course.name)
+                            .bold()
+                    }
+                    .padding(3)
+                }
+            }
+            .navigationTitle("Courses")
+            .onAppear {
+                viewModel.fetch()
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleAppAPI/SampleAppAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppAPIApp.swift
+//  SampleAppAPI
+//
+//  Created by Cindy Michalowski on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
